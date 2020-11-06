7231710dbcc77d4260b17e6a5c194b66564b8344
Anthony Belcastro <belcastt@live.com>
2020-11-05 22:53:02 -0700

    done

--- src/App.js
+++ src/App.js
@@ -3,9 +3,9 @@ import { BrowserRouter as Router, Route } from 'react-router-dom';
 import { Container } from 'react-bootstrap';
 import Header from './components/Header';
 import Footer from './components/Footer';
-import HomePage from './Pages/HomePage';
-import ProductPage from './Pages/ProductPage';
-import products from './products';
+import ProductPage from './pages/ProductPage';
+import HomePage from './pages/HomePage';
+// import products from './products';
 
 
 const App = () => {
