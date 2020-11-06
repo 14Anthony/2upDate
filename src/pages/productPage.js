c794b893820bb54012be0a1a1ac0b1d0b67e2441
Anthony Belcastro <belcastt@live.com>
2020-11-05 21:26:32 -0700

    creating a project page

--- src/pages/productPage.js
+++ src/pages/productPage.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
-import { Row, Col, Image, ListGroup, Card, Button } from 'react-bootstrap';
+import { Row, Col, Image, ListGroup, Card, Button, Container } from 'react-bootstrap';
 import Rating from '../components/Rating';
 import products from '../products';
 
@@ -8,9 +8,37 @@ const ProductPage = ({ match }) => {
     const product = products.find((p) => p._id === match.params.id)
     return (
         <>
-            <Link className='btn btn-warning my-3 ' to='/'>
+            <Link className='btn btn-grey my-3 ' to='/'>
                 Home
         </Link>
+            <Container>
+                <Row>
+                    <Col md={6}>
+                        <Image src={product.image} alt={product.name} fluid />
+                    </Col>
+                    <Col md={3}>
+                        <ListGroup variant='flush' >
+                            <ListGroup.Item>
+                                <h2>{product.name}</h2>
+                            </ListGroup.Item>
+                            <ListGroup.Item>
+                                <strong>Some Peopler Like it?:</strong>
+                                <Rating
+                                    value={product.rating}
+                                    text={`${product.numReviews}`} />
+                            </ListGroup.Item>
+
+                            <ListGroup.Item>
+                                <strong>Project Description:</strong> <br></br><br></br> {product.description}
+                            </ListGroup.Item>
+
+
+                        </ ListGroup>
+
+                    </Col>
+                </Row>
+
+            </Container>
         </ >
     )
 }
