a43acff62444f1dcd50865aba1d338f501352cae
Anthony Belcastro <belcastt@live.com>
2020-11-05 22:35:27 -0700

    blackmagic: :

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Route } from 'react-router-dom';
 import { Container } from 'react-bootstrap';
 import Header from './components/Header';
 import Footer from './components/Footer';
-import HomePage from './Pages/HomePage.js';
+import HomePage from './Pages/HomePage';
 import ProductPage from './Pages/ProductPage';
 // import products from './products';
 
